bd77382d4ffc90af5f200f4a3fa9449983ed8a79
Walker Silvestre <walker_contas@hotmail.com>
2023-05-09 14:35:30 -0300

    Exercise 10: What Was That?

---
+++ ex10.rb
@@ -0,0 +1,18 @@
+"I am 6'2\" tall."
+'I am 6\'2" tall.'
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backlash_cat
+puts fat_cat
