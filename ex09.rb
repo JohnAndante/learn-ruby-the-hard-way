23c193f931a013930a122bec60c4faac41ef5c5b
Walker Silvestre <walker_contas@hotmail.com>
2023-05-09 14:35:20 -0300

    Exercise 9: Printing, Printing, Printing

---
+++ ex09.rb
@@ -0,0 +1,14 @@
+# here's some new strange stuff, remenber type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here
+With the PARAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
