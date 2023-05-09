1f17b945e17896881f96bd519226fea2351be358
Walker Silvestre <walker_contas@hotmail.com>
2023-05-09 14:32:54 -0300

    Exercise 01: A Good First Program

---
+++ ex01.rb
@@ -0,0 +1,11 @@
+puts "Hello, world!"
+puts "Hello Again."
+puts "I like typing this stuff."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I's muuch rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Another line printed."
+
+# Annotation here
+
