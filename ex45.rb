f06ba4f379a44463251e0129af2e2325e1167f47
Walker Silvestre <walker_contas@hotmail.com>
2023-05-16 16:58:18 -0300

    Update ex45.rb

--- ex45.rb
+++ ex45.rb
@@ -3,30 +3,30 @@ class Animal
 
 end
 
-## ??
+## Dog is-a Animal, Dog is-a Class
 class Dog < Animal
 
   def initialize(name)
-    ## ??
+    ## Dog has-a name
     @name = name
   end
 end
 
-## ??
+## Cat is-a Animal, Cat is-a Class
 class Cat < Animal
 
   def initialize(name)
-    ## ??
+    ## Cat is-a name
     @name = name
   end
 end
 
-## ??
+## Person is-a class
 class Person
   attr_accessor :pet
 
   def initialize(name)
-    ## ??
+    ## Person has-a name
     @name = name
 
     ## Person has-a pet of some kind
@@ -34,18 +34,20 @@ class Person
   end
 end
 
-## ??
+## Employee is-a Person
 class Employee < Person
 
   def initialize(name, salary)
-    ## ?? hmm what is this strange magic?
+    ## Calls the name of the Person class
+    ## Better saying, the name variable is stored in the Person class
+    ## instead of the Employee class
     super(name)
-    ## ??
+    ## Person has-a salary
     @salary = salary
   end
 end
 
-## ??
+##
 class Fish
 
 end
